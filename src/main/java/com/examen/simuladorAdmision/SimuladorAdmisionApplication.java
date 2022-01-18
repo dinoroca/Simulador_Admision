8ea0f174aeccfd571f136f2a7e71b746a178405f
Dino Roca <rocalaurented@gmail.com>
2022-01-18 16:05:51 -0500

    first commit

---
+++ src/main/java/com/examen/simuladorAdmision/SimuladorAdmisionApplication.java
@@ -0,0 +1,13 @@
+package com.examen.simuladorAdmision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorAdmisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorAdmisionApplication.class, args);
+	}
+
+}
